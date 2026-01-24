0d78347446211098694d6921b0501299f0fe072b
ocelot <konradc699@gmail.com>
2026-01-24 13:23:16 +0100

    esxcersiuces

---
+++ petle/petle1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+int x;
+int y;
+int i = {0};
+int arr[3] = {5,10,15}; 
+
+
+
+for (i = 0; i < 3; i++ )
+  {
+   std::cout << "i wynosi: " << arr[i] << std::endl;
+  }
+
+
+
+
+
+
+
+
+
+return 0;
+}
