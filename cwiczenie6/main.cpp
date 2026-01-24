8a9ece1b3400614582c5ad401aab34dd2775306c
ocelot <konradc699@gmail.com>
2026-01-24 12:25:27 +0100

    some excersices

--- cwiczenie6/main.cpp
+++ cwiczenie6/main.cpp
@@ -10,12 +10,13 @@ double mydouble = 3.14;
 double& myreference = mydouble;
 int x;
 int y;
-double* p = mydouble;
+double* p = &mydouble;
 
 myreference = 6.28;
 
 std::cout  << "values:"  << mydouble << "and" << myreference << std::endl; 
 
+std::cout << "pointer value" << *p  << std::endl;
 
 mydouble = 9.27;
 
@@ -23,6 +24,20 @@ mydouble = 9.27;
 std::cout  << "values:"  << mydouble << "and" << myreference << std::endl; 
 
 std::cout << "pointer value" << *p  << std::endl;
+
+std::string tofind = "world";
+std::string s = "Hello world";
+
+std::string::size_type found = s.find(tofind);
+if(found != std::string::npos)
+{
+std::cout << "substring found at position: " << found << std::endl;
+}
+else 
+{
+std::cout << "substring wasnt found" << found << std::endl;
+}
+
 
 
 
