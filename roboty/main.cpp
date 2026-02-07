acc2d60822595d8eaa043f8c0beaa8d0f611216e
NegativeAura619 <konradc699@gmail.com>
2026-02-07 23:46:08 +0100

    i dont know anything anymore

--- roboty/main.cpp
+++ roboty/main.cpp
@@ -5,18 +5,22 @@
 class robot
 {
 public:
+ int x,y;
+ robot(int xx, int yy) : x(xx), y(yy)
+  {
+    move();
+    x = 0;
+    y = 0;
+  }
+
 void move();
-
 };
-
-
-
 
 int main()
 {
 
 
-robot i;
+robot o{0,0};
 
 
 return 0;
@@ -24,5 +28,5 @@ return 0;
 
 void robot::move()
 {
-std::cout << "helloworld fromaclass" << std::endl;
+std::cout << "Initialized, starting position: " << x << y <<std::endl;
 }
