5225b642699b1a94dee77a05c964a3e576a2bfe0
NegativeAura619 <konradc699@gmail.com>
2026-02-07 23:34:48 +0100

    added code for testing robot moving, and classes, still too hard for me

---
+++ roboty/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+class robot
+{
+public:
+void move();
+
+};
+
+
+
+
+int main()
+{
+
+
+robot i;
+
+
+return 0;
+}
+
+void robot::move()
+{
+std::cout << "helloworld fromaclass" << std::endl;
+}
